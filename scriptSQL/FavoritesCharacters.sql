abb659710d0aead21104078b7deb4fa007505245
Gabriel Wottawa <gabrielclerio10@gmail.com>
2022-07-11 23:08:53 -0300

    Ajuste no scriptSql

--- scriptSQL/FavoritesCharacters.sql
+++ scriptSQL/FavoritesCharacters.sql
@@ -1,3 +1,14 @@
+USE master
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE database_id = DB_ID(N'Marvel')) BEGIN
+	CREATE DATABASE Marvel
+END
+GO
+
+USE Marvel
+GO
+
 IF (OBJECT_ID('FavoritesCharacters')) IS NULL BEGIN
 	CREATE TABLE FavoritesCharacters (
 		Id INT identity(1,1) NOT NULL PRIMARY KEY,
